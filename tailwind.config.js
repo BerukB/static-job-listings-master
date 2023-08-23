2fdade5b3a2b0f1c7edac22f4e749ca0653ac127
BerukB <berukberhanua@gmail.com>
2023-08-23 05:10:59 -0700

    basic front end finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,17 @@ module.exports = {
     screens: {
       sm: "375px",
       md: "768px",
-      lg: "1280px",
+      lg: "976px",
       xl: "1440px",
     },
     extend: {
+      backgroundImage: {
+        desktopHeader: "url('/images/bg-header-desktop.svg')",
+        mobileHeader: "url('/images/bg-header-mobile.svg')",
+      },
       colors: {
         desaturatedDarkCyan: "hsl(180, 29%, 50%)",
-        lightGrayishCyan: "hsl(180, 52%, 96%)",
+        lightGrayishCyanBg: "hsl(180, 52%, 96%)",
         lightGrayishCyan: "hsl(180, 31%, 95%)",
         darkGrayishCyan: "hsl(180, 8%, 52%)",
         veryDarkGrayishCyan: "hsl(180, 14%, 20%)",
@@ -23,6 +27,9 @@ module.exports = {
         regular: "500",
         bold: "700",
       },
+      fontSize: {
+        newSize: ['14px'],
+      }
     },
   },
   plugins: [],
